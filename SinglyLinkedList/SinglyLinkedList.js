70361b77a5495eafd21fac615c485929035a79f7
Sergei Liddell <liddell.s@pm.me>
2021-06-28 12:24:10 -0600

    Modifies code to trump initial !head check

--- SinglyLinkedList/SinglyLinkedList.js
+++ SinglyLinkedList/SinglyLinkedList.js
@@ -83,15 +83,14 @@ class SinglyLinkedList {
     currentNode.next = null;
   }
   hasCycle(head = this.head) {
-    if (!head) return false;
     let slow = head;
-    let fast = head.next;
-    while (fast !== null) {
+    let fast = head;
+    while (fast !== null && fast.next !== null) {
+      slow = slow.next;
+      fast = fast.next.next;
       if (fast === slow) {
         return true;
       }
-      slow = slow.next;
-      fast = fast.next.next;
     }
     return false;
   }
