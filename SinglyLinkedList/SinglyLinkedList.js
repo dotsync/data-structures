52f451ae1ba218f83c59ba28aecf4f992cec2864
Sergei Liddell <liddell.s@pm.me>
2021-06-27 18:04:09 -0600

    Implements addToHead method for SinglyLinkedList

--- SinglyLinkedList/SinglyLinkedList.js
+++ SinglyLinkedList/SinglyLinkedList.js
@@ -1,9 +1,28 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 class SinglyLinkedList {
   constructor() {
     this.head = null;
-    this.tail = null;
-    this.length = 0;
+    this.size = 0;
   }
+  // addToHead
+  addToHead(value) {
+    if (this.head === null) {
+      this.head = new Node(value);
+      this.size++;
+    } else {
+      const oldHead = this.head;
+      this.head = new Node(value);
+      this.head.next = oldHead;
+      this.size++;
+    }
+  }
+  // addToTail
+  // print
 }
 
 module.exports = SinglyLinkedList;
--- SinglyLinkedList/SinglyLinkedList.test.js
+++ SinglyLinkedList/SinglyLinkedList.test.js
@@ -1,16 +1,41 @@
 const SinglyLinkedList = require('./SinglyLinkedList');
 
+/**
+
+  Singly Linked List
+  node1    |---> node2
+  data     |     data
+  next --->      next ---> null
+
+notes:
+Singly linked lists do not have a tail
+ */
+
 describe('Singly Linked List Class', () => {
-  describe('Basic functionality of class', () => {
+  describe('Basic functionality of class test suite', () => {
     const ll = new SinglyLinkedList();
     test('should have a head equal to null', () => {
       expect(ll.head).toBeNull();
     });
-    test('should have a tail equal to null', () => {
-      expect(ll.tail).toBeNull();
+    test('should have a size equal to 0', () => {
+      expect(ll.size).toBe(0);
     });
-    test('should have a length equal to 0', () => {
-      expect(ll.length).toBe(0);
+  });
+  describe('addToHead method test suite', () => {
+    const ll = new SinglyLinkedList();
+    ll.addToHead('New York');
+    test('should add new value to head', () => {
+      console.log(ll);
+      expect(ll.head.value).toBe('New York');
+      expect(ll.head.next).toBeNull();
+      expect(ll.size).toBe(1);
+    });
+    test('should add a second value to the head', () => {
+      ll.addToHead('Connecticut');
+      expect(ll.head.value).toBe('Connecticut');
+      expect(ll.head.next.value).toBe('New York');
+      expect(ll.head.next.next).toBeNull();
+      expect(ll.size).toBe(2);
     });
   });
 });
